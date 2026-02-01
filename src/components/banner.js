0f3f204039aae156bd064e28d96a34a71083e7b3
Mathias BERGER <mathias.codage@gmail.com>
2026-02-01 15:46:42 +0100

    🎨 shoppingList & ➕ Recommendation.js

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,14 +1,18 @@
 import '../styles/banner.css'
 import logo from '../assets/logo.png'
+import Recommandation from './recommendation'
 
 function Banner() {
     const title = 'La maison jungle'
     return (
         <div className='lmj-banner'>
-            <img src={logo} alt='La maison jungle' className='lmj-logo' />
-            <h1 className='lmj-title'>{title}</h1>
+            <div className='lmj-banner-row>'>
+                <img src={logo} alt='La maison jungle' className='lmj-logo' />
+                <h1 className='lmj-title'>{title}</h1>
+            </div>
+            <Recommandation />
         </div>
-        )
+    )
 }
 
 export default Banner
