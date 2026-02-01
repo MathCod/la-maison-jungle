bc352c641ee327ac25031f30364d88b084cff630
Mathias BERGER <mathias.codage@gmail.com>
2026-02-01 16:26:57 +0100

    Ajustement 🎨 Banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -6,11 +6,13 @@ function Banner() {
     const title = 'La maison jungle'
     return (
         <div className='lmj-banner'>
-            <div className='lmj-banner-row>'>
+            <div className='lmj-banner-row'>
                 <img src={logo} alt='La maison jungle' className='lmj-logo' />
                 <h1 className='lmj-title'>{title}</h1>
             </div>
-            <Recommandation />
+                <div className='lmj-recommendation'>
+                    <Recommandation />
+                </div>
         </div>
     )
 }
